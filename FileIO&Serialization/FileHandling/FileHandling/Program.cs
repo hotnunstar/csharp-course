f9e58a9cfcd6a8d16c420c0cc5d771983e028963
Nuno Araújo <nu-nos@hotmail.com>
2024-09-27 11:25:24 +0100

    File I/O & Serialization

---
+++ FileIO&Serialization/FileHandling/FileHandling/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling
+{
+    internal class Program
+    {
+        [Serializable]
+        class Counter
+        {
+            public int Value { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Counter counter;
+
+            try
+            {
+                using (FileStream fs = new FileStream("counter.dat", FileMode.OpenOrCreate))
+                {
+                    if (fs.Length > 0)
+                    {
+                        BinaryFormatter formatter = new BinaryFormatter();
+                        counter = (Counter)formatter.Deserialize(fs);
+                        Console.WriteLine("Successfully deserialized counter.");
+                    }
+                    else
+                    {
+                        counter = new Counter();
+                        Console.WriteLine("File is empty. Initializing new counter");
+                    }
+                }
+            }
+            catch (SerializationException ex)
+            {
+                Console.WriteLine($"SerializationException: {ex.Message}");
+                counter = new Counter();
+            }
+            catch (FileNotFoundException)
+            {
+                counter = new Counter();
+                Console.WriteLine("File not found. Initializing new counter.");
+            }
+
+            Console.WriteLine($"Current Value: {counter.Value}");
+            counter.Value++;
+
+            using (FileStream fs = new FileStream("counter.dat", FileMode.Create))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(fs, counter);
+                Console.WriteLine("Successfully serialized counter");
+            }
+        }
+    }
+}
---
+++ FileIO&Serialization/JSON&XML/JSON&XML/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace JSON_XML
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonData = new Person { Name = "John", Age = 30 };
+            File.WriteAllText("data.json", JsonConvert.SerializeObject(jsonData));
+
+            var jsonString = File.ReadAllText("data.json");
+            var jsonPerson = JsonConvert.DeserializeObject<Person>(jsonString);
+            Console.WriteLine($"JSON Data: Name={jsonPerson.Name}, Age={jsonPerson.Age}");
+
+            var xmlData = new Person { Name = "Jane", Age = 25 };
+            var xmlSerializer = new XmlSerializer(xmlData.GetType());
+
+            using (var write = new StreamWriter("data.xml"))
+            {
+                xmlSerializer.Serialize(write, xmlData);
+            }
+
+            using (var reader = new StreamReader("data.xml"))
+            {
+                var xmlObj = xmlSerializer.Deserialize(reader) as Person;
+                Console.WriteLine($"XML Data: Name={xmlObj.Name}, Age={xmlObj.Age}");
+            }
+        }
+    }
+}
