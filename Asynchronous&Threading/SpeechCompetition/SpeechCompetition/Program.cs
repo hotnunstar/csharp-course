d0069f9f8f7bb477d5a7904bdabc28b4773e7b12
Nuno Araújo <nu-nos@hotmail.com>
2024-09-30 10:36:23 +0100

    Asynchronous & Threading

---
+++ Asynchronous&Threading/SpeechCompetition/SpeechCompetition/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpeechCompetition
+{
+    internal class Program
+    {
+        static int participantsCount = 0;
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread t = new Thread(EnterSpeechCompetition);
+                t.Start(i);
+            }
+        }
+
+        static void EnterSpeechCompetition(object participantId)
+        {
+            Monitor.Enter(typeof(Program));
+            try
+            {
+                participantsCount++;
+                Console.WriteLine($"Participant {participantId} entered. Total participants: {participantsCount}");
+            }
+            finally
+            {
+                Monitor.Exit(typeof(Program));
+            }
+
+            Thread.Sleep(1000);
+            Monitor.Enter(typeof(Program));
+
+            try
+            {
+                participantsCount--;
+                Console.WriteLine($"Participant {participantId} finished. Total participants: {participantsCount}");
+            }
+            finally
+            {
+                Monitor.Exit(typeof(Program));
+            }
+        }
+    }
+}
---
+++ Asynchronous&Threading/Thread/Thread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread
+{
+    internal class Program
+    {
+        static List<Student> GetStudentData() =>
+            Enumerable.Range(1, 10).Select(i => new Student
+            {
+                ID = i,
+                Name = $"Student-{i}"
+            }).ToList();
+
+        static async Task<int> ProcessStudentAsync(Student student)
+        {
+            await Task.Delay(100);
+            Console.WriteLine($"Processing data for {student.Name}");
+            return student.ID * 2;
+        }
+
+        static async Task Main()
+        {
+            var students = GetStudentData();
+            var results = await Task.WhenAll(students.Select(ProcessStudentAsync));
+            Console.WriteLine($"Total processed data: {results.Sum()}");
+        }
+    }
+}
